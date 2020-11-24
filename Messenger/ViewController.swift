5602e3f4a28c9aa98f5e5ed125568a2d8440df09
kimjunseong <kimjunseong@archive-asia.co.jp>
2020-11-24 13:50:13 +0900

    getting started

---
+++ Messenger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Messenger
+//
+//  Created by kimjunseong on 2020/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
